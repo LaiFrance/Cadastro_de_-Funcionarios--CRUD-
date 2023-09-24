2e3366f3fb2e1329ba3323cf9dde58a9a982f982
Laise France <134656901+laisefarias@users.noreply.github.com>
2023-09-24 06:16:36 -0300

    alterações

--- api/db.js
+++ api/db.js
@@ -1,10 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: process.env.DB_HOST,         // Use a variável de ambiente para o host
-    user: process.env.DB_USER,         // Use a variável de ambiente para o usuário
-    password: process.env.DB_PASSWORD, // Use a variável de ambiente para a senha
-    database: process.env.DB_DATABASE,  // Use a variável de ambiente para o nome do banco de dados
-    port: process.env.DB_PORT          // Use a variável de ambiente para a porta
+    host: "localhost",
+    user: "root",
+    password: "851514",
+    database: "cadastro_de_funcionarios",
 })
-
