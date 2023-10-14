2e6eda1b41da3242a69a170262d47efb340f9b14
Laise France <134656901+laisefarias@users.noreply.github.com>
2023-10-13 21:07:56 -0300

    add modificações

--- api/db.js
+++ api/db.js
@@ -1,11 +1,8 @@
-import mysql from "mysql";
-require("dotenv").config();
-
-const port = process.env.PORT || 3000;
+import mysql from "mysql"
 
 export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+    host: "localhost",
+    user: "root",
+    password: "851514",
+    database: "cadastro_de_funcionarios",
+})
--- api/index.js
+++ api/index.js
@@ -1,24 +1,21 @@
-import express from "express";
-import { db } from "./db.js";
-import userRoutes from "./routes/users.js";
-import cors from "cors";
-require("dotenv").config();
+import express from "express"
+import {db} from "./db.js"
+import userRoutes from "./routes/users.js"
+import cors from "cors"
 
-const port = process.env.DB_PORT || 8800;
+const app = express()
 
-const app = express();
+app.use(express.json())
+app.use(cors())
 
-app.use(express.json());
-app.use(cors());
+app.use("/", userRoutes)
 
-app.use("/", userRoutes);
-
-app.listen(port); // Porta 8800
+app.listen(8800) // Porta 8800
 
 // message conexao com o banco de dados
-db.connect(err => {
-  if (err) {
-    console.log("Erro ao conectar ao banco de dados");
-  }
-  console.log("Conectado ao banco de dados");
+db.connect((err) => {
+    if (err) {
+        console.log("Erro ao conectar ao banco de dados");
+    }
+    console.log("Conectado ao banco de dados");
 });
